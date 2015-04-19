1dab670a824920b35b3ea6a4751b0fb99694a67f
Issei Naruta <naruta@cookpad.com>
2015-04-19 15:09:39 +0900

    Implement some options

--- lib/fav2reblog/engine.rb
+++ lib/fav2reblog/engine.rb
@@ -20,31 +20,31 @@ module Fav2reblog
       id
     end
 
-    def last_id=(id)
+    def set_last_id(id, update_file: true)
       path = Fav2reblog.config['position_file']
-      open(path, 'w') {|f| f.write id }
+      open(path, 'w') {|f| f.write id } if update_file
       id
     end
 
-    def execute
+    def execute(dry_run: false, update_last_id: true)
       _last_id = last_id.to_i
       logger.debug("execute: last_id=#{_last_id}")
       @twitter.favorites_with_media.each do |tweet|
         next if _last_id >= tweet.id
-        reblog tweet
+        reblog tweet, dry_run: dry_run, update_last_id: update_last_id
       end
     rescue
       logger.error "#{$!.class}: #{$!}\n  #{$@.join("\n  ")}"
     end
 
-    def reblog(tweet)
+    def reblog(tweet, dry_run: false, update_last_id: true)
       image_files = tweet.media.map {|m| open(m.media_uri) }
       data = image_files.map {|f| f.path }
       caption = %Q(Twitter / #{tweet.user.screen_name}: #{tweet.full_text})
       link = tweet.uri
       logger.info("reblog: id=#{tweet.id}, data=#{data}, caption=#{caption}, link=#{link}")
-      @tumblr.post_photo data: data, caption: caption, link: link
-      self.last_id = tweet.id
+      @tumblr.post_photo data: data, caption: caption, link: link unless dry_run
+      set_last_id tweet.id, update_file: update_last_id
     end
 
     def logger
